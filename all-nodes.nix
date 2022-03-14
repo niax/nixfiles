cd5effc9b4d56cd03e33a801bab16c0929e5008a
Nicholas Hollett <niax@niax.co.uk>
2022-03-14 19:12:48 +0000

    Move home/user things out of the all-nodes spec

--- all-nodes.nix
+++ all-nodes.nix
@@ -8,11 +8,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "niax";
-  home.homeDirectory = "/home/niax";
-
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
