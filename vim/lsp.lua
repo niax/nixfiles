760541f5bd49fe1ede32c6b36c17dd089febc8eb
Nicholas Hollett <niax@niax.co.uk>
2022-11-03 17:20:37 -0700

    Fix nvim LSP deprecated function call

--- vim/lsp.lua
+++ vim/lsp.lua
@@ -67,7 +67,7 @@ local on_attach = function(client, bufnr)
 end
 
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local servers = {
   "gopls",
   "pyright",
