9f9f2b452bbd082f5795e81752f33d4ab9b7b607
Nicholas Hollett <niax@niax.co.uk>
2026-04-03 16:51:43 +0000

    Try some NixOS on WSL

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "niax home-manager";
+  description = "niax nixfiles";
 
   inputs = {
     claude-code.url = "github:sadjow/claude-code-nix";
@@ -8,9 +8,13 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, claude-code, ... }:
+  outputs = { nixpkgs, nixos-wsl, home-manager, claude-code, ... }:
     let
       # Helper to build a homeConfiguration for a given system + module
       mkHome = system: module:
@@ -29,6 +33,41 @@
 
         # Mac
         "niax@lightcycle" = mkHome "aarch64-darwin" ./home-mac.nix;
+      };
+      nixosConfigurations."hotel-hightower" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          home-manager.nixosModules.home-manager
+          ({pkgs, ...}: {
+            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+            wsl.enable = true;
+            wsl.defaultUser = "niax";
+            system.stateVersion = "25.11"; # NO TOUCH ME!
+            networking.hostName = "hotel-hightower";
+
+            environment.systemPackages = with pkgs; [
+              git
+              vim
+            ];
+            programs.zsh.enable = true;
+
+            users.users.niax = {
+              isNormalUser = true;
+              home = "/home/niax";
+              description = "niax";
+              extraGroups = [
+                "wheel"
+              ];
+              shell = pkgs.zsh;
+            };
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.niax = ./home-windows.nix;
+            };
+          })
+        ];
       };
     };
 }
