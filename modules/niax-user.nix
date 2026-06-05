3d7690b59aa984be3fdde446c72417a18635836c
Nicholas Hollett <niax@niax.co.uk>
2026-06-04 20:15:35 -0700

    Add lightcycle's public key

--- modules/niax-user.nix
+++ modules/niax-user.nix
@@ -8,6 +8,7 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMI6YMzipLPfoK9iX1jDIY1KGwsxh5K2eta+gk5jwGx big-thunder"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7VHoCSmHl3P53AYSAsDBVxV4v9qTyaYqnp/a4KMy9z lightcycle"
     ];
   };
 }
